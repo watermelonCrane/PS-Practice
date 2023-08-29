bc0761c8dc19e2db407e77d6aea09005c8f4f1bc
kimjaehak <kimjaehak@ajou.ac.kr>
2023-08-29 22:41:13 +0900

    230829

---
+++ 1764.cpp
@@ -0,0 +1,41 @@
+//정답
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    
+    map<string, bool, less<string>> m;
+    map<string, bool>::iterator it;
+    string str;
+    while (N--) {
+        cin >> str;
+        m[str] = false;
+    }
+    while (M--) {
+        cin >> str;
+        it = m.find(str);
+        if (it != m.end()) {
+            it->second = true;
+        }
+    }
+
+    vector<string> list;
+    int su = 0;
+    for (auto& x : m) {
+        if (x.second) {
+            su++;
+            list.push_back(x.first);
+        }
+    }
+    cout << su << "\n";
+    for (auto& x : list) {
+        cout << x << "\n";
+    }
+
+    return 0;
+}

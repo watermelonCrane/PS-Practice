23a81eef600e1e05ff64fc22eaec17a9864755b0
kimjaehak <kimjaehak@ajou.ac.kr>
2023-09-03 13:44:36 +0900

    230903

---
+++ 15649.cpp
@@ -0,0 +1,39 @@
+//백트래킹???
+//쨌든 성공
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void NM(int N, int M, vector<int>& v) {
+    if (v.size() == M) {
+        for (auto& x: v) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    else {
+        for (int i = 1; i <= N; i++) {
+            if (find(v.begin(), v.end(), i) != v.end()) {
+                continue;
+            }
+            v.push_back(i);
+            NM(N, M, v);
+        }
+    }
+    v.pop_back();
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> v;
+
+    NM(N, M, v);
+    
+
+    return 0;
+}
